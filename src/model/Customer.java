589eb25c4fa12e98a8f2e9ea72e4785cc345b960
kishorekumar-kk <40785931+kishorekumar-kk@users.noreply.github.com>
2023-06-28 17:27:22 +0200

    Added methods to save rentals and points for customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,14 +1,35 @@
 package model;
 
+import java.util.UUID;
+
 public class Customer {
     private final String name;
 
+
+    private final String customerGUID;
+
+    private Integer loyaltyPoints;
+
     public Customer(String name) {
         this.name = name;
+        this.loyaltyPoints = 0;
+        this.customerGUID = UUID.randomUUID().toString();
+
     }
 
     public String getName() {
         return name;
     }
 
+    public void addLoyaltyPoints(Integer points) {
+        this.loyaltyPoints += points;
+    }
+
+    public String getCustomerGUID() {
+        return customerGUID;
+    }
+
+    public Integer getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
 }
